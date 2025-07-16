db80984d00fd5f1d24ee1e5345ebede803bbaec7
uhsr <uhsr@eteb.me>
2025-07-16 19:03:10 +0700

    Add volatileeye.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/volatileeye/volatileeye.go
@@ -0,0 +1,36 @@
+// internal/volatileeye/volatileeye.go
+package volatileeye
+
+import (
+"log"
+"os"
+)
+
+type App struct {
+verbose bool
+logger *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+app := &App{
+verbose: verbose,
+logger: log.New(os.Stdout, "", log.LstdFlags),
+}
+
+if verbose {
+app.logger.SetPrefix("[DEBUG] ")
+} else {
+app.logger.SetPrefix("[INFO] ")
+}
+
+return app
+}
+
+func (a *App) Run() error {
+a.logger.Printf("Starting %s processing", "VolatileEye")
+
+// Add your implementation here
+
+a.logger.Println("Processing completed successfully")
+return nil
+}
